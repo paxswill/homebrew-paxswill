8cdd008a33a108283114fb651c24d4d66a1b6f4a
Will Ross <wross@cmf.nrl.navy.mil>
2012-08-17 10:53:55 -0400

    Rework the arcanist formula to allow `arc upgrade`
    It also removes the requirement to include `--HEAD`. Basically, it
    preserves the git repositories for both arcanist and libphutil.
    
    Note: by explicitly allowing `arc upgrade`, this formula is not eligible
    for merging into the main Homebrew repository (unless they decide to
    make an exception, but I think that's unlikely).

--- arcanist.rb
+++ arcanist.rb
@@ -2,14 +2,24 @@ require 'formula'
 
 class Arcanist < Formula
   homepage 'http://phabricator.org/'
-  head 'https://github.com/facebook/arcanist.git'
+  url 'https://github.com/facebook/arcanist.git'
+  version 'tip'
 
   def install
-    cd 'externals/includes' do
+    # Instead of copying things piecemeal, I just reclone arcanist in.
+    # This let's `arc upgrade` work (which is also a viloation of Homebrew
+    # policies).
+    system 'git', 'clone', url
+    cd 'arcanist/externals/includes' do
       system 'git', 'clone', 'git://github.com/facebook/libphutil.git'
     end
-    (prefix+'etc/bash_completion.d').install "resources/shell/bash-completion" => 'arcanist'
-    bin.install 'bin/arc'
-    prefix.install "externals", "scripts", "src"
+    prefix.install 'arcanist'
+    completion_d = prefix+'etc/bash_completion.d'
+    mkdir_p completion_d
+    ln_s (prefix+'arcanist/resources/shell/bash-completion'), (completion_d+'arcanist')
+    (bin+'arc').write <<-EOS.undent
+    #!/bin/sh
+    "#{prefix}/arcanist/bin/arc" "$@"
+    EOS
   end
 end
